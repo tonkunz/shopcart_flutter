85d2a321ddcb2952ddadd90a7b4f389707af67ce
Everton <ehokeverton@gmail.com>
2020-03-09 00:05:50 -0400

    Modify bloc to provide Home

--- lib/blocs/home_bloc.dart
+++ lib/blocs/home_bloc.dart
@@ -2,8 +2,9 @@ import '../models/category_model.dart';
 import '../models/product_list_item.dart';
 import '../repositories/category_repository.dart';
 import '../repositories/product_repository.dart';
+import 'package:flutter/widgets.dart';
 
-class HomeBloc {
+class HomeBloc extends ChangeNotifier {
   final categoryRepo = new CategoryRepository();
   final productRepo = new ProductRepository();
 
@@ -18,22 +19,32 @@ class HomeBloc {
 
   getCategories() {
     categoryRepo.getAll()
-      .then((data) => this.categories = data);
+      .then((data) {
+        this.categories = data;
+         notifyListeners();
+      }); 
   }
 
   getProducts() {
     productRepo.getAll()
-      .then((data) => this.products = data);
+      .then((data) {
+        this.products = data;
+        notifyListeners();
+      });
   }
 
   getProductsByCategory() {
     productRepo.getByCategory(selectedCategory)
-      .then((data) => this.products = data);
+      .then((data) {
+        this.products = data;
+        notifyListeners();
+      });
   }
 
   changeCategory(tag) {
     selectedCategory = tag;
     products = null;
     getProductsByCategory();
+    notifyListeners();
   }
 }
