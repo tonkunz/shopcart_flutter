f3adecc9a23ec6cf1e6afe0fd4faa9d47c08c01e
Everton Kunz <ehokeverton@gmail.com>
2020-03-18 14:04:49 -0400

    Add item quantity handle buttons on CartPage

--- lib/layouts/android/pages/tabs_page.dart
+++ lib/layouts/android/pages/tabs_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopcart_bloc_provider/blocs/cart_bloc.dart';
 import 'package:shopcart_bloc_provider/layouts/android/pages/cart_page.dart';
 import 'home_page.dart';
 
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CartBloc bloc = Provider.of<CartBloc>(context);
+
     return Scaffold(
       body: TabBarView(
         children: [
@@ -19,7 +23,28 @@ class TabsPage extends StatelessWidget {
             icon: new Icon(Icons.home),
           ),
           Tab(
-            icon: new Icon(Icons.shopping_cart),
+            icon: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.shopping_cart),
+                Container(
+                  width: 18,
+                  height: 18,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                  ),
+                  child: Text(
+                    bloc.cart.length.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
           ),
           Tab(
             icon: new Icon(Icons.perm_identity),
--- lib/layouts/shared/widgets/cart/cart_item_widget.dart
+++ lib/layouts/shared/widgets/cart/cart_item_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:shopcart_bloc_provider/blocs/cart_bloc.dart';
 import 'package:shopcart_bloc_provider/models/cart_item.dart';
 
 class CartItem extends StatelessWidget {
@@ -8,6 +11,9 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final price = new NumberFormat("#,##0.00", "pt_BR");
+    CartBloc bloc = Provider.of<CartBloc>(context);
+
     return Container(
       height: 120,
       margin: EdgeInsets.all(5),
@@ -20,6 +26,64 @@ class CartItem extends StatelessWidget {
             child: Image.network(
               item.image,
               fit: BoxFit.fitWidth,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 20,
+              left: 10,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(item.title),
+                Text(
+                  "R\$ ${price.format(item.price)}",
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text("R\$ ${price.format((item.price * item.quantity))}"),
+                Container(
+                  height: 30,
+                  width: 120,
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 40,
+                        alignment: Alignment.center,
+                        child: FlatButton(
+                          child: Text("-"),
+                          onPressed: () {
+                            bloc.decrease(item);
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Text(item.quantity.toString()),
+                        ),
+                      ),
+                      Container(
+                        width: 40,
+                        alignment: Alignment.center,
+                        child: FlatButton(
+                          child: Text("+"),
+                          onPressed: () {
+                            bloc.increase(item);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ],
