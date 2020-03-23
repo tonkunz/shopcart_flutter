5e09ae12a4b3df69dfd45c3c30e9c955395fefe2
Everton <ehokeverton@gmail.com>
2020-03-22 22:19:55 -0400

    Add signup feature

--- lib/layouts/android/pages/signup_page.dart
+++ lib/layouts/android/pages/signup_page.dart
@@ -1,10 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopcart_bloc_provider/blocs/account_bloc.dart';
+import 'package:shopcart_bloc_provider/layouts/shared/validators/custom_validator.dart';
+import 'package:shopcart_bloc_provider/models/create_user_model.dart';
 
-class SignupPage extends StatelessWidget {
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  var user = new CreateUserModel();
+
+  create(BuildContext context) async {
+    final AccountBloc bloc = Provider.of<AccountBloc>(context, listen: false);
+    var res = await bloc.create(user);
+
+    if (res == null) {
+      final snackBar =
+        SnackBar(content: Text("Não foi possível realizar seu cadastro"));
+      _scaffoldKey.currentState.showSnackBar(snackBar);
+    } else {
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: "Nome",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  )
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Nome Inválido";
+                  }
+                  return null;
+                },
+                onSaved: (val) => user.name = val,
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  )
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+                validator: (value) => CustomValidators.isEmail(value),
+                onSaved: (val) => user.email = val,
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: "Usuário",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Usuário Inválido";
+                  }
+                  return null;
+                },
+                onSaved: (val) => user.username = val,
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Senha inválida";
+                  }
+                  return null;
+                },
+                onSaved: (val) => user.password = val,
+              ),
+              FlatButton(
+                child: Text("Cadastrar"),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    _formKey.currentState.save();
+                    create(context);
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
